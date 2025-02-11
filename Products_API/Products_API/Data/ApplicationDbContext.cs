adf61b49afe3ecb1527d39950584b7c41943ff0c
Brunda M <brunda.m@csgkarnataka.in>
2025-02-11 21:25:41 +0530

    Created Database and Table using code first approch.

---
+++ Products_API/Products_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products_API.Models.Entities;
+
+namespace Products_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Products_API/Products_API/Models/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Products_API.Models.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
